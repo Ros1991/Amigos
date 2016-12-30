68d9d4772c11799738a06ba042516667f805e6ee
Rodrigo <rodrigo.silva@veriz.com.br>
2016-12-30 11:55:40 -0200

    a

--- src/app/dashboard/games/games.component.ts
+++ src/app/dashboard/games/games.component.ts
@@ -5,19 +5,31 @@ import './owlCarousel/owl.carousel.min.js';
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
-  styleUrls: ['./games.component.scss', './owlCarousel/owl.carousel.css', './owlCarousel/owl.theme.default.css']
+  styleUrls: ['./games.component.scss', './owlCarousel/docs.theme.min.css','./owlCarousel/owl.carousel.min.css', './owlCarousel/owl.theme.default.css']
 })
 
 export class GamesComponent {
     constructor() {
-        
     }
 
     ngOnInit() {
         $(document).ready(function () {
-            $('.owl-carousel').owlCarousel();
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 3
+                    },
+                    1000: {
+                        items: 5
+                    }
+                }
+            })
         });
     }
-        
-    
 }
